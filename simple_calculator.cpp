985eb7aaa6b14714ef295727cfaaa8cc82e573fb
Domanowska <domanowskaw@gmail.com>
2021-08-16 14:31:32 -0400

    Change cpp to use while loop instead of continue

--- simple_calculator.cpp
+++ simple_calculator.cpp
@@ -2,55 +2,57 @@
 
 using namespace std;
 
-int main()
+main()
 {
-    double a, b;
-
-    beginning:
-
-    cout << "Enter first number: " << endl;
-    cin >> a;
-    cout << "Enter second number: " << endl;
-    cin >> b;
-
-    cout << "What mathematical operation do you want to perform? " << endl;
-    cout << "+ : addition" << endl;
-    cout << "- : subtraction" << endl;
-    cout << "* : multiplication" << endl;
-    cout << "/ : division" << endl;
-
-    char operation;
-    cin >> operation;
-
-    // Clear console
-    system("clear"); // "cls" on windows
-
-    switch(operation)
+    char calculate = 'Y';
+    do 
     {
-        case '+':
-            cout << a << " + " << b << " = " << a + b << endl;
-            break;
-        case '-':
-            cout << a << " - " << b << " = " << a - b << endl;
-            break;
-        case '*':
-            cout << a << " * " << b << " = " << a * b << endl;
-            break;
-        case '/':
-            if (b)
-                cout << a << " / " << b << " = " << a / b << endl;
-            else
-                cout << "Cannot divide by 0!" << endl;
-            break;
-        default:
-            cout << "That operation is not supported!" << endl;
-    }
+        cin.clear();
+        cout.clear();
+        fflush(stdin);
+        fflush(stdout);
+
+        double a, b;
+
+        cout << "Enter first number: " << endl;
+        cin >> a;
+        cout << "Enter second number: " << endl;
+        cin >> b;
+
+        cout << "What mathematical operation do you want to perform? " << endl;
+        cout << "+ : addition" << endl;
+        cout << "- : subtraction" << endl;
+        cout << "* : multiplication" << endl;
+        cout << "/ : division" << endl;
+
+        char operation;
+        cin >> operation;
+
+        // Clear console
+        system("cls"); // "clear" on MacOS
+
+        switch(operation)
+        {
+            case '+':
+                cout << a << " + " << b << " = " << a + b << endl;
+                break;
+            case '-':
+                cout << a << " - " << b << " = " << a - b << endl;
+                break;
+            case '*':
+                cout << a << " * " << b << " = " << a * b << endl;
+                break;
+            case '/':
+                if (b)
+                    cout << a << " / " << b << " = " << a / b << endl;
+                else
+                    cout << "Cannot divide by 0!" << endl;
+                break;
+            default:
+                cout << "That operation is not supported!" << endl;
+        }
     
-    cout << "Continue calculating? (Y/N) " << endl;
-    char keep_going;
-    cin >> keep_going;
-
-    if (keep_going == 'y' || keep_going == 'Y')
-        goto beginning; // DO NOT USE INSTEAD OF LOOPS
-
+        cout << "Continue calculating? (Y/N) " << endl;
+        cin >> calculate;
+    } while (calculate == 'Y' || calculate == 'y');
 }
